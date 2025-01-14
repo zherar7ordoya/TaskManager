5bf7c70a708315f1aaa4a69a9c68e7de5a98730b
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-01-14 01:36:32 -0300

    Taller ASP NET CORE MVC Clase1 Video 2

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task = TaskManager.Models.Task;
+
+namespace TaskManager.Controllers;
+
+public class TaskController : Controller
+{
+    private static readonly List<Task> _tasks = [];
+    private static int _nextId = 1;
+
+    public IActionResult Index()
+    {
+        return View(_tasks);
+    }
+
+    public IActionResult Detail(int id)
+    {
+        Task? task = _tasks.FirstOrDefault(t => t.Id == id);
+        if (task == null)
+        {
+            return NotFound();
+        }
+        return View(task);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Task task)
+    {
+        if (ModelState.IsValid)
+        {
+            task.Id = _nextId++;
+            _tasks.Add(task);
+            return RedirectToAction("Index");
+        }
+        return View(task);
+    }
+}
---
+++ Models/Task.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskManager.Models;
+
+public class Task
+{
+    public int Id { get; set; }
+    public string Title { get; set; }= string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public bool IsCompleted { get; set; }
+}
